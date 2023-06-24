bdae7e2d8f6748659bcebed7556d9572c4e79e4d
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-24 16:06:06 -0300

    remove unsed

--- src/test/java/br/com/raiosystems/apirest/course/GetAllCourseServiceTest.java
+++ src/test/java/br/com/raiosystems/apirest/course/GetAllCourseServiceTest.java
@@ -1,6 +1,5 @@
 package br.com.raiosystems.apirest.course;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
