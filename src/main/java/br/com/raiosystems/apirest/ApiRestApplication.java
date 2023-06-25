291b9c0687be59cd748c32a051690734288bd42e
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-25 17:25:46 -0300

    app route.

--- src/main/java/br/com/raiosystems/apirest/ApiRestApplication.java
+++ src/main/java/br/com/raiosystems/apirest/ApiRestApplication.java
@@ -2,6 +2,9 @@ package br.com.raiosystems.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiRestApplication {
@@ -9,6 +12,17 @@ public class ApiRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 		System.out.println("# - STATUS: Rest is running at [ http://localhost:8080 ]");
+	}
+
+	@RestController
+	@RequestMapping("/")
+	public class AppController {
+
+		@GetMapping
+		public Object getApp() {
+			return "<center><h3>HelloWorld!</h3>";
+		}
+
 	}
 
 }
