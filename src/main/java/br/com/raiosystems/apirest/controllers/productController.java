335110f1e6812f7dfa3cd08c83bedd93f2650c36
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-25 17:19:10 -0300

    get product route

--- src/main/java/br/com/raiosystems/apirest/controllers/productController.java
+++ src/main/java/br/com/raiosystems/apirest/controllers/productController.java
@@ -1,16 +1,20 @@
 package br.com.raiosystems.apirest.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.raiosystems.apirest.entities.Course;
 import br.com.raiosystems.apirest.entities.Product;
 import br.com.raiosystems.apirest.modules.products.services.CreateProductService;
 import br.com.raiosystems.apirest.modules.products.services.GetAllProductService;
+import br.com.raiosystems.apirest.modules.products.services.GetProductService;
 import br.com.raiosystems.apirest.repositories.ProductRepositoryTest;
 
 @RestController
@@ -20,6 +24,7 @@ public class ProductController {
   ProductRepositoryTest repository = new ProductRepositoryTest();
   private CreateProductService createProductService = new CreateProductService(repository);
   private GetAllProductService getAllProductService = new GetAllProductService(repository);
+  private GetProductService getProductService = new GetProductService(repository);
 
   @PostMapping
   public Product createProduct(@RequestBody Product product) {
@@ -30,6 +35,12 @@ public class ProductController {
   @GetMapping
   public List<Product> listProducts() {
     List<Product> response = this.getAllProductService.execute();
+    return response;
+  }
+
+  @GetMapping("/")
+  public Product getProduct(@RequestParam UUID id) {
+    Product response = this.getProductService.execute(id);
     return response;
   }
 
