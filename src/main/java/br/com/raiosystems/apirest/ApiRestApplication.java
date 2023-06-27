2369fc61dfe6fc1533c31caee308bbc19146b307
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-26 22:25:03 -0300

    POSTGRES RUNNN!

--- src/main/java/br/com/raiosystems/apirest/ApiRestApplication.java
+++ src/main/java/br/com/raiosystems/apirest/ApiRestApplication.java
@@ -7,13 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sound.midi.Soundbank;
+
 @SpringBootApplication
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 		System.out.println("# - STATUS: Rest is running at [ http://localhost:8080 ]");
-		Conection.execute();
+
+		try {
+			Conection.execute();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	@RestController
--- src/main/java/br/com/raiosystems/apirest/database/Conection.java
+++ src/main/java/br/com/raiosystems/apirest/database/Conection.java
@@ -10,7 +10,7 @@ public class Conection {
       Connection conn = null;
         try {
           conn = DriverManager.getConnection("jdbc:postgresql://localhost/JAVA", "postgres", "thinklife");
-          System.out.println("Connected to the PostgreSQL server successfully.");
+          System.out.println("# - STATUS: Connected to the PostgreSQL server successfully.");
           return conn;
         } catch (SQLException e) {
           System.out.println(e.getMessage());
